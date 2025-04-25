b0c0966c1607f58a8c3c01d870f3bd70c7fbd19b
VuDao381 <daoanhvu2005hp@gmail.com>
2025-04-25 08:09:21 +0000

    quanlirapphim Fixes #1

---
+++ quanlirapfilm/src/Phim.java
@@ -0,0 +1,8 @@
+public class Phim {
+    private int id;
+    private String tenPhim;
+    private String theLoai;
+    private int thoiLuong;
+    private String daoDien;
+    private String dienVien;
+}
