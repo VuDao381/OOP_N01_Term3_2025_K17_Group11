48e6abd205f6c141a8af3cf79c2953037b1e4952
VuDao381 <daoanhvu2005hp@gmail.com>
2025-05-14 11:25:53 +0000

    bbb

--- Library/test/UserTest.java
+++ Library/test/UserTest.java
@@ -1,6 +1,7 @@
 public class UserTest {
     public static void main(String[] args) throws Exception {
-    User newUser = new User("Alex","abc@gmail.com", "123456");
-    newUser.displayUserInfo();
-}
+    User user1 = new User("Alex","abc@gmail.com", "123456");
+    System.out.println("Email: "+ user1.getEmail());
+    
+    }
 }
