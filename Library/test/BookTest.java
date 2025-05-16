80fad31fd4e2483af7dd03aae818d18043f81242
VuDao381 <daoanhvu2005hp@gmail.com>
2025-05-16 08:36:44 +0000

    aaaa

--- Library/test/BookTest.java
+++ Library/test/BookTest.java
@@ -1,6 +1,6 @@
 public class BookTest {
     public static void TestBook(){
-        Book book1 = new Book("Meitantei Conan","Aoyama Gosho","Kim Đồng",100);
+        Book book1 = new Book("Detective Conan","Aoyama Goushou","Kim Đồng",100);
         System.out.println("Title: "+ book1.getTitle());
         System.out.println("Author: "+ book1.getAuthor());
         System.out.println("Publisher: "+ book1.getPublisher());
--- Library/test/BorrowSlipTest.java
+++ Library/test/BorrowSlipTest.java
@@ -1,6 +1,6 @@
 public class BorrowSlipTest {
     public static void testBorrowSlip(){
-    BorrowSlip slip1 = new BorrowSlip("Conan","Alex","1/1/2025","1/2/2015");    
+    BorrowSlip slip1 = new BorrowSlip("Detective Conan","Alex","1/1/2025","1/2/2015");    
         System.out.println("Book Title: " + slip1.getBookTitle());
         System.out.println("User Name: " + slip1.getUserName());
         System.out.println("Borrow Date: " + slip1.getBorrowDate());
