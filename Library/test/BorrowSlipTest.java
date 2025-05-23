1e8b23e56e4827cab67589f33732f6cfe76dfc93
VuDao381 <daoanhvu2005hp@gmail.com>
2025-05-23 13:32:04 +0700

    BorrowSlipTest.java

--- Library/test/BorrowSlipTest.java
+++ Library/test/BorrowSlipTest.java
@@ -1,6 +1,6 @@
 public class BorrowSlipTest {
     public static void testBorrowSlip(){
-    BorrowSlip slip1 = new BorrowSlip("Detective Conan","Alex","1/1/2025","1/2/2015");    
+    BorrowSlip slip1 = new BorrowSlip("Detective Conan","Alex","1/1/2025","1/2/2025");    
         System.out.println("Book Title: " + slip1.getBookTitle());
         System.out.println("User Name: " + slip1.getUserName());
         System.out.println("Borrow Date: " + slip1.getBorrowDate());
