677909d2bd9ea76e26a7fc4464102e71719e68e7
khachung-05 <23017320@st.phenikaa-uni.edu.vn>
2025-05-30 06:53:37 +0000

    muon sach

---
+++ Library/src/BookBorrowingRecord.java
@@ -0,0 +1,22 @@
+public class BookBorrowingRecord{
+
+    /*
+     * to do:
+     * Ghi nhan muon sach -->User sach
+     * Cap nhat tinh trang sach
+     */
+
+
+     public void MuonSach(){
+
+     }
+     public Book SachStatus(){
+
+        Book book = new Book();
+
+        //cap nhap book
+
+        return book;
+     }
+
+}
